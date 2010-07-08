525d802863f3d32adc8b25024803e1c7e7fbaf7a
Mark Veltzer <mark.veltzer@gmail.com>
2010-07-09 02:26:15 +0300

    example is much better now

--- user_space/stamping_binaries/gcc_date_macro.cc
+++ user_space/stamping_binaries/gcc_date_macro.cc
@@ -17,10 +17,17 @@
 
 #include "us_helper.hh"
 
-// the static allows us to use a compiled on tag for each file!!!
-//static char* __attribute__((unused)) date=(char*)__FILE__ " compiled on " __DATE__ " " __TIME__;
-//const char* date=__FILE__ " compiled on " __DATE__ " " __TIME__;
-//static int __attribute__((unused)) a=7;
+#define __stringify_1(x) # x
+#define __stringify(x)   __stringify_1(x)
+
+#define STRING_VERSION "1.23.56"
+#define NUMERIC_VERSION 1.23.56
+
+// the static allows us to use a compiled on tag for each file so you can put it in a common
+// header and get stamping for all files in your project.
+static const char* __attribute__((used)) date="date=" __FILE__ " " __stringify(__LINE__) " "__DATE__ " " __TIME__;
+static const char* __attribute__((used)) string_version="string_version=" STRING_VERSION;
+static const char* __attribute__((used)) numeric_version="numeric_version=" __stringify(NUMERIC_VERSION);
 
 int main(int argc, char **argv, char **envp) {
 	printf("date is %s\n", __DATE__);
@@ -29,5 +36,11 @@ int main(int argc, char **argv, char **envp) {
 	scpe(strptime(__DATE__ " " __TIME__, "%b %d %Y %H:%M:%S", &tm), NULL);
 	time_t t = mktime(&tm);
 	printf("t is %lu\n", t);
+
+	// lets try to run strings(1) on our own binary to see the data...
+	const unsigned int len=1024;
+	char cmd[len];
+	snprintf(cmd,len,"strings %s | grep =",argv[0]);
+	scie(system(cmd),"system");
 	return(0);
 }
