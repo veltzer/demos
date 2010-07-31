c96f6792898d2d2b1062d6fb3cae150e06a4ef85
Mark Veltzer <mark.veltzer@gmail.com>
2010-07-31 17:56:59 +0300

    improvements

--- php/agile/ArrayTest.php
+++ php/agile/ArrayTest.php
@@ -2,23 +2,20 @@
 require_once 'PHPUnit/Framework/TestCase.php';
 
 class ArrayTest extends PHPUnit_Framework_TestCase {
-    public function testNewArrayIsEmpty() {
-        // Create the Array fixture.
-        $fixture = Array();
+	public function testNewArrayIsEmpty() {
+		// Create the Array fixture.
+		$fixture = Array();
+		// Assert that the size of the Array fixture is 0.
+		$this->assertEquals(0, sizeof($fixture));
+	}
 
-        // Assert that the size of the Array fixture is 0.
-        $this->assertEquals(0, sizeof($fixture));
-    }
-
-    public function testArrayContainsAnElement() {
-        // Create the Array fixture.
-        $fixture = Array();
-
-        // Add an element to the Array fixture.
-        $fixture[] = 'Element';
-
-        // Assert that the size of the Array fixture is 1.
-        $this->assertEquals(1, sizeof($fixture));
-    }
+	public function testArrayContainsAnElement() {
+		// Create the Array fixture.
+		$fixture = Array();
+		// Add an element to the Array fixture.
+		$fixture[] = 'Element';
+		// Assert that the size of the Array fixture is 1.
+		$this->assertEquals(2, sizeof($fixture));
+	}
 }
 ?>
