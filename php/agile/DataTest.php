41dc31d6f29e2333fec3f72fa51fc24a154b5922
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-08 11:01:22 +0300

    some php renames

--- php/agile/DataTest.php
+++ php/agile/DataTest.php
@@ -6,7 +6,7 @@ class DataTest extends PHPUnit_Framework_TestCase {
 	* @dataProvider provider
 	*/
 	public function testAdd($a, $b, $c) {
-	$this->assertEquals($c, $a + $b);
+		$this->assertEquals($c, $a + $b);
 	}
 	     
 	public function provider() {
--- php/agile/DependencyTest.php
+++ php/agile/DependencyTest.php
@@ -1,10 +1,11 @@
 <?php
 require_once 'PHPUnit/Framework.php';
 
-class DependencyFailureTest extends PHPUnit_Framework_TestCase
+class DependencyTest extends PHPUnit_Framework_TestCase
 {
 	public function testOne() {
 		$this->assertTrue(TRUE);
+		//$this->assertTrue(FALSE);
 	}
 	     
 	/**
