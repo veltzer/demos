f704ffd3fe84e445f9c78204edb9e788819c87d5
Mark Veltzer <mark.veltzer@gmail.com>
2010-08-22 02:25:12 +0300

    improved the popup example a lot

--- javascript/jquery/popup.js
+++ javascript/jquery/popup.js
@@ -1,8 +1,11 @@
 // TODO:
-// - fading of the background color.
-// - don't allow interaction with the background
-// - don't allow more than one popup.
-// - fade in/fade out of the popups instead of regular show/hide.
+// - document this example in detail.
+//
+// This is based on this article:
+// http://yensdesign.com/2008/09/how-to-create-a-stunning-and-smooth-popup-using-jquery/
+
+var popupShown=null;
+var opacityLevel=0.7;
 
 jQuery.fn.extend({
 	center: function() {
@@ -14,7 +17,49 @@ jQuery.fn.extend({
 			{
 				"position": "absolute",
 				"top": windowHeight/2-popupHeight/2,
-				"left": windowWidth/2-popupWidth/2
+				"left": windowWidth/2-popupWidth/2,
+			}
+		);
+	},
+	fullScreen: function() {
+		var windowWidth = document.documentElement.clientWidth;
+		var windowHeight = document.documentElement.clientHeight;
+		$(this).css({
+			"position": "fixed",
+			"width": windowWidth,
+			"height": windowHeight,
+			"z-index": 1,
+			"top": 0,
+			"left": 0,
+		});
+	},
+	onTop: function() {
+		$(this).css({
+			"z-index":2,
+		});
+	},
+	popupShow: function() {
+		$(this).fadeIn("slow");
+	},
+	popupHide: function() {
+		$(this).fadeOut("slow");
+	},
+	popupFadeIn: function() {
+		$($(this).attr('popupSelector')).fadeIn("slow");
+		//$($(this).attr('bgSelector')).fadeIn("slow");
+		$($(this).attr('bgSelector')).show().animate({opacity:opacityLevel},{
+			duration: 200,
+			//complete: function() { alert("in finish"); },
+			}
+		);
+	},
+	popupFadeOut: function() {
+		$($(this).attr('popupSelector')).fadeOut("slow");
+		//$($(this).attr('bgSelector')).fadeOut("slow");
+		var selector=$($(this).attr('bgSelector'));
+		selector.animate({opacity:0},{
+			duration: 200,
+			complete: function() { selector.hide(); },
 			}
 		);
 	}
@@ -24,16 +69,71 @@ $(document).ready(function() {
 	// hide and center the popup
 	$(".popup").hide();
 	$(".popup").center();
+	$(".popup").onTop();
+	$(".popup").css({
+		"background": "#FFFFFF",
+		"border": "2px solid #cecece",
+	});
+	$(".popupBg").hide();
+	$(".popupBg").fullScreen();
+	$(".popupBg").css({
+		"opacity": "0.0",
+		"background": "#000000"
+	});
 	$(".popupShow").click(function() {
-		$($(this).attr('popupSelector')).show();
+		if(popupShown!=null) {
+			$(popupShown.attr('popupSelector')).popupHide();
+		}
+		popupShown=$(this);
+		$(popupShown.attr('popupSelector')).popupShow();
 		}
 	);
 	$(".popupHide").click(function() {
-		$($(this).attr('popupSelector')).hide();
+		if(popupShown!=null) {
+			$(popupShown.attr('popupSelector')).popupHide();
+			popupShown=null;
+		}
 		}
 	);
 	$(".popupToggle").click(function() {
-		$($(this).attr('popupSelector')).toggle();
+		if(popupShown!=null) {
+			$(popupShown.attr('popupSelector')).popupHide();
+			popupShown=null;
+		} else {
+			popupShown=$(this);
+			$(popupShown.attr('popupSelector')).popupShow();
+		}
+		//$($(this).attr('popupSelector')).toggle();
 		}
 	);
+	$(".popupFadeIn").click(function() {
+		if(popupShown!=null) {
+			popupShown.popupFadeOut();
+			popupShown=null;
+		}
+		popupShown=$(this);
+		popupShown.popupFadeIn();
+		}
+	);
+	$(".popupFadeOut").click(function() {
+		if(popupShown!=null) {
+			popupShown.popupFadeOut();
+			popupShown=null;
+		}
+		}
+	);
+	$(document).keydown(function(e) {
+		//alert("e.keyCode is "+e.keyCode);
+		if(e.keyCode==27 && popupShown!=null) {
+			popupShown.popupFadeOut();
+			popupShown=null;
+		}
+	});
+	$(document).keypress(function(e) {
+		//alert("e.keyCode is "+e.keyCode);
+		if(e.keyCode==27 && popupShown!=null) {
+			popupShown.popupFadeOut();
+			popupShown=null;
+		}
+	});
 });
