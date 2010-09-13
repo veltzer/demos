7a3b337d1b2b43ef8dd84a8d56b70e977c9893b6
Mark Veltzer <mark.veltzer@gmail.com>
2010-09-13 10:45:21 +0200

    added python embedding example

---
+++ python/examples/long/embedding_python_in_c/embed.c
@@ -0,0 +1,12 @@
+#include <Python.h>
+int main(int argc,char** argv,char** envp) {
+	Py_Initialize();
+	PyRun_SimpleString(
+		"print \"Hello, World!\"\n"
+		"import time\n"
+		"time.sleep(60)\n"
+		"print \"Woke up!\"\n"
+	);
+	Py_Finalize();
+	return 0;
+}
