b47a4cece1678d182823cf74aea5ffd8836b8fb4
Mark Veltzer <mark.veltzer@gmail.com>
2010-10-26 15:18:18 +0200

    finished the script

--- scripts/get_extra_files.pl
+++ scripts/get_extra_files.pl
@@ -12,7 +12,7 @@ use diagnostics;
 use File::Find qw();
 
 # do you want to debug the script ?!?
-my($debug)=1;
+my($debug)=0;
 my(%hash)=();
 
 sub handler() {
@@ -31,3 +31,18 @@ sub handler() {
 
 # find all files in the folder...
 File::Find::find({"no_chdir"=>1,"wanted"=>\&handler},".");
+
+open(FILE,"git ls-files |") || die("unable to run git");
+my($line);
+while($line=<FILE>) {
+	chop($line);
+	if($debug) {
+		print 'saw line ['.$line.']'."\n";
+	}
+	delete($hash{'./'.$line});
+}
+close(FILE) || die("unable to close git");
+
+while(my($file,$val)=each %hash) {
+	print 'extra file ['.$file.']'."\n";
+}
