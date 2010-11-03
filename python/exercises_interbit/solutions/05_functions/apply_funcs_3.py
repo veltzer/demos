852d22a969a5e61c12c0f2658691206956887623
Mark Veltzer <mark.veltzer@gmail.com>
2010-11-03 15:52:29 +0200

    some more fixes

--- python/exercises_interbit/solutions/05_functions/apply_funcs_3.py
+++ python/exercises_interbit/solutions/05_functions/apply_funcs_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
 def apply_funcs(funcs, x):
-    """Apply a list of unary functions on an argument,
-        Return the result"""
-    return [f(x) for f in funcs]
+	"""Apply a list of unary functions on an argument,
+	Return the result"""
+	return [f(x) for f in funcs]
--- python/exercises_interbit/solutions/05_functions/swap_lists_2.py
+++ python/exercises_interbit/solutions/05_functions/swap_lists_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 def swap_lists(l1,l2):
-    for i in xrange(len(l1)):
-        l1[i],l2[i] = l2[i],l1[i]
+	for i in xrange(len(l1)):
+		l1[i],l2[i] = l2[i],l1[i]
 
 l1=[1,2,3]
 l2=[4,5,6]
