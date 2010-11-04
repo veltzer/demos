48eab9b926da3c78cd920080001232296a70746a
Mark Veltzer <mark.veltzer@gmail.com>
2010-11-04 11:12:45 +0200

    latest changes

--- python/exercises_interbit/solutions/05_functions/min_max_avg_2.py
+++ python/exercises_interbit/solutions/05_functions/min_max_avg_2.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python
 
-def max_min_avg(* num):
-	"return a tuple containing the maximnum, minimum and avrage of the given numbrs"
-	return max(num), min(num), sum(num)/len(num)
+def my_min(list):
+	min=list[0]
+	for x in list[1,]:
+		if x<min:
+			min=x
+	return min
 
-print max_min_avg(2,3,4)
+def my_max(list):
+	max=list[0]
+	for x in list[1,]:
+		if x>max:
+			max=x
+	return max
+
+def my_sum(list):
+	sum=0
+	for x in list[1,]:
+		sum+=x
+	return sum 
+
+def min_max_avg(list):
+	return my_min(list),my_max(list),float(my_sum(list))/len(list)
+
+l=range(0,100000)
+print min_max_avg(l)
