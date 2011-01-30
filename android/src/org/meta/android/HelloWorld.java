5aac43b86ab6b9ef31a277f7a5460e8c9aa2f711
Mark Veltzer <mark.veltzer@gmail.com>
2011-01-30 18:56:05 +0200

    added another application

--- android/src/org/meta/android/HelloWorld.java
+++ android/src/org/meta/android/HelloWorld.java
@@ -3,7 +3,7 @@ package org.meta.android;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SayHello extends Activity {
+public class HelloWorld extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
