49ac04be3727d28b75888f6ef12505eccdd35897
Mark Veltzer <mark.veltzer@gmail.com>
2011-04-07 11:26:55 +0300

    commit

---
+++ python/exercises/programming_old/03_basic_statements/rev_hash_4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+print "Starting..."
+
+def my_apply(f,seq):
+	for x in seq:
+		f(x)
+
+def reverse_hash(d):
+	target={}
+	my_apply(lambda k: target.__setitem__(d[k],k),d)
+	# this will create a compilation error
+	#my_apply(lambda k: target[d[k]]=k,orig)
+	return target
+
+orig = {"Israel":"Jerusalem","France":"Paris","Italy":"Rome","Egypt":"Cairo"}
+print reverse_hash(orig)
