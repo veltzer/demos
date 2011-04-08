c3cb5c17958d9246671d0d8173e9efe69eff0d6b
Mark Veltzer <mark.veltzer@gmail.com>
2011-04-08 07:30:13 +0300

    more changes to names

--- python/examples/short/system.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-os.system("ls -l")
-print "Im still here"
-os.execl("/bin/ls");
-print "Where am I?"
---
+++ python/examples/short/system_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+"""
+This example demonstrates the use of the 'exec' family of functions. Notice that after
+you call them you are no longer running (your executable get replaced or rather the python
+virtual machine gets replaced...).
+"""
+
+import os
+
+os.system("ls -l")
+print "Im still here"
+os.execl("/bin/ls");
+print "Where am I?"
