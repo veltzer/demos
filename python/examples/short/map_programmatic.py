5c255385764f7e2025ca986d65d744f9bd28d53f
Mark Veltzer <mark.veltzer@gmail.com>
2011-04-09 06:52:59 +0300

    more stuff

--- python/examples/short/map_programmatic.py
+++ python/examples/short/map_programmatic.py
@@ -3,12 +3,15 @@
 """
 this is an example of implementing the python builtin 'map'
 function in python.
+
+Obviously you should not use this approach and it is presented
+for pedagogic purposes only. Python's own 'map' is written in C
+and performs much better.
 """
 def my_map(f,seq):
 	l=[]
 	for x in seq:
 		l.append(f(x))
 	return l
-
 
 print my_map(lambda x:x*x,range(10))
--- python/examples/short/overload_functions.py
+++ python/examples/short/overload_functions.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
+"""
+This example shows that you cannot overload functions in python.
+The second function 'overwrites' the first.
+
+There is a kind of overloading in python using named arguments and
+variable arguments but not of this kind.
+
+Functions also share the same namespace with regular variables. So,
+in this example, defining a variable named 'foo' would override the
+function so that it cannot be used.
+"""
+
 def foo():
 	print "hello"
 
