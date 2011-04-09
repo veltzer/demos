5d3f819e88341e0cf410eb26c6ba6443bf521ee9
Mark Veltzer <mark.veltzer@gmail.com>
2011-04-09 06:23:53 +0300

    improved the object oriented example

--- python/examples/short/object_oriented.py
+++ python/examples/short/object_oriented.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
-class Book:
-	__price=0
-	def printit(self):
-		print 'price is',self.__price
-	def setPrice(self,newprice):
-		self.__price=newprice
+
+""" This is a fairly simple basic OO example """
+class Book(object):
+	""" constructor """
 	def __init__(self,price):
 		self.__price=price
+	def getPrice(self):
+		return self.__price
+	def setPrice(self,newprice):
+		self.__price=newprice
+	def printMe(self):
+		print 'price is',self.__price
+
+""" Lets show how we use our object... """
+b=Book(50)
+b.printMe()
+b.setPrice(60)
+b.printMe()
