7c0203f169e6d86a2173389dee023eb826daa6dd
Mark Veltzer <mark.veltzer@gmail.com>
2011-04-30 06:50:10 +0300

    added an example of calling a function by name

---
+++ php/examples/call_by_name.php
@@ -0,0 +1,16 @@
+<html>
+<!--
+	This is an example of how to call a function by name
+-->
+<body>
+	<?php
+	function add($a,$b) {
+		return $a+$b;
+	}
+	function printit($c) {
+		echo $c."\n";
+	}
+	call_user_func('printit',call_user_func('add',2,2));
+	?>
+</body>
+</html>
