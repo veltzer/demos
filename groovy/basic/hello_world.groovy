6d866a887510392d2fffe888c3bca4a14bb2d176
Mark Veltzer <mark.veltzer@gmail.com>
2011-05-07 21:08:21 +0300

    more examples

--- groovy/basic/hello_world.groovy
+++ groovy/basic/hello_world.groovy
@@ -4,6 +4,15 @@
 // As you can see, line starting with // are comments.
 
 /*
+	notice that:
+	- groovy source files are usually called .groovy
+	- you don't HAVE to write OO code in groovy (this is similar to
+	other scripting languages like Perl, Python and more).
+	- this makes it easy for sys admins and programmers to use groovy
+	for small tasks.
+*/
+
+/*
 	This is
 	a
 	multi
---
+++ groovy/basic/hello_world_oo.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/groovy
+
+/*
+*/
+
+/*
+	This is an example of writing oo groovy code.
+	The entry point, like in regular Java is the static
+	method main.
+*/
+
+class foo {
+	static void main(args) {
+		println "Hello, World! "+args;
+	}
+}
