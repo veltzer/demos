2a820b8084c273cd17f0ee87f3940298d47705cd
Mark Veltzer <mark.veltzer@gmail.com>
2011-05-08 07:07:12 +0300

    more libs

--- java/src/groovy/embed/BSF.java
+++ java/src/groovy/embed/BSF.java
@@ -1,10 +1,21 @@
 package groovy.embed;
 
+import java.util.List;
+
+import org.apache.bsf.BSFException;
+import org.apache.bsf.BSFManager;
+
 public class BSF {
+	@SuppressWarnings("unchecked")
 	public static void main(String args[]) {
 		String myScript = "println('Hello World')\n  return [1, 2, 3]";
 		BSFManager manager = new BSFManager();
-		List answer = (List) manager.eval("groovy", "myScript.groovy", 0, 0, myScript);
-		assertEquals(3, answer.size());
+		List<Integer> answer;
+		try {
+			answer = (List<Integer>) manager.eval("groovy", "myScript.groovy", 0, 0, myScript);
+		} catch (BSFException e) {
+			throw new RuntimeException(e);
+		}
+		assert(3==answer.size());
 	}
 }
