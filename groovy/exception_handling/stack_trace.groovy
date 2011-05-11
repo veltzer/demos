a87a2d6098a83227af0c6e29018f45f8870cfd45
Mark Veltzer <mark.veltzer@gmail.com>
2011-05-11 04:13:09 +0300

    more stuff

--- groovy/exception_handling/stack_trace.groovy
+++ groovy/exception_handling/stack_trace.groovy
@@ -5,9 +5,12 @@
 	are runtime (no checked exceptions). Some find that a very good idea...
 */
 
+import org.codehaus.groovy.runtime.StackTraceUtils 
+
 try {
 	throw new RuntimeException("fubar")
 } catch(RuntimeException e) {
 	println "yes, we got an exception"
-	println "exception is "+e.printStackTrace()
+	e.printStackTrace()
+	println "Wasnt that a long stack trace ?"
 }
