3d00ea7ad86b6e08d99ce69974d4e08dd8041ff1
Mark Veltzer <mark.veltzer@gmail.com>
2011-05-19 07:37:02 +0300

    meta programming tips

---
+++ groovy/meta_programming/add_methods_to_third_party_classes.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/groovy
+
+/*
+	This is an example of how to add methods to a class that you did not write...
+	In this case we add methods to the Integer class
+
+	Notes:
+	- Inside the closure which is attached to the metaClass class the 'delegate' automatic
+	variable is the instance on which the method is called.
+	- 'this' is the closure object itself.
+*/
+
+
+Integer x=5
+println x
+
+try {
+	println x.minusOne()
+} catch(MissingMethodException e) {
+	println 'no, the minusOne method does not exist (yet)'
+}
+println x.metaClass
+x.metaClass.minusOne={ return delegate-1 }
+println x.minusOne()
