9cfff6e3d0d90e198e39b406311a3918fd411365
Mark Veltzer <mark.veltzer@gmail.com>
2011-05-19 04:43:22 +0300

    more examples for modules

---
+++ groovy/modules/user.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/groovy
+
+/*
+*/
+
+// lets import the 'A' module...
+
+import A
+
+println A.myStaticMethod(2,2)
+try {
+	println A.myNonStaticMethod(2,2)
+} catch(MissingMethodException e) {
+	println 'yes, got exception for a missing method'
+}
