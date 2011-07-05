33aeef3dc5ff9485b573c33c3fd63941df8e23d6
Mark Veltzer <mark.veltzer@gmail.com>
2011-07-05 10:13:36 +0300

    another example

--- python/examples/short/single_line.py
+++ python/examples/short/single_line.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 
 """
+	This example shows that you can put code on the same line as
+	a black starter...
+"""
+
+"""
 	Mark Veltzer <mark@veltzer.net>
 """
 if True: print "show"
 else:
 	print "Else"
+
+for x in xrange(10): print x
