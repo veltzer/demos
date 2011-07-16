8b53a8ea79fb95bdd29c1ac71cdc9e709705c06a
Mark Veltzer <mark.veltzer@gmail.com>
2011-07-16 17:12:41 +0300

    organization of the php subfolder

--- php/examples/file_upload_page.php
+++ php/examples/file_upload_page.php
@@ -1,6 +1,6 @@
 <html>
 	<body>
-		<form action="file_upload.php" method="post" enctype="multipart/form-data">
+		<form action="file_upload_server.php" method="post" enctype="multipart/form-data">
 			<input type="hidden" name="MAX_FILE_SIZE" value="3000000"/>
 			<input type="file" name="upload"/>
 			<input type="submit" value="Upload"/>
