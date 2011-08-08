c0a8306642a5e09ad1bd1111882c1340a3e588e4
Mark Veltzer <mark.veltzer@gmail.com>
2011-08-08 07:50:35 +0300

    more modifications

--- java/scripts/ubuntu_install.sh
+++ java/scripts/ubuntu_install.sh
@@ -3,4 +3,11 @@
 # this scrip will install all the required packages that you need on
 # ubuntu to compile and work with this package.
 
-sudo apt-get install visualvm openjdk-6-jdk
+# the application -> packages that I need are:
+# jvisualvm -> visualvm
+# jmap -> openjdk-6-jdk
+# jhat -> openjdk-6-jdk
+# jconsole -> openjdk-6-jdk
+# netbeans -> netbeans
+
+sudo apt-get install visualvm openjdk-6-jdk netbeans
