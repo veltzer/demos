742e42248da2eecfdc3a4eb860b0da3e87d9c4a1
Mark Veltzer <mark.veltzer@gmail.com>
2011-12-16 02:36:00 +0200

    more fixes

--- javascript/ext4/paging_simple/paging.js
+++ javascript/ext4/paging_simple/paging.js
@@ -1,17 +1,3 @@
-/*
-
-This file is part of Ext JS 4
-
-Copyright (c) 2011 Sencha Inc
-
-Contact:  http://www.sencha.com/contact
-
-GNU General Public License Usage
-This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
-
-If you are unsure which license is appropriate for your use, please contact the sales department at http://www.sencha.com/contact.
-
-*/
 Ext.onReady(function(){
 	Ext.define('MovieModel', {
 		extend: 'Ext.data.Model',
@@ -21,7 +7,7 @@ Ext.onReady(function(){
 			],
 		idProperty: 'id'
 	});
-	var store=Ext.create('Ext.data.Store',{
+	var w_store=Ext.create('Ext.data.Store',{
 		pageSize: 50,
 		model: 'MovieModel',
 		proxy: {
@@ -34,10 +20,9 @@ Ext.onReady(function(){
 			},
 		},
 	});
-	var grid=Ext.create('Ext.grid.Panel',{
-		height: 500,
+	var w_grid=Ext.create('Ext.grid.Panel',{
 		title: 'Movies that I have seen',
-		store: store,
+		store: w_store,
 		columns:[
 			{
 				text: "Id",
@@ -53,7 +38,7 @@ Ext.onReady(function(){
 			},
 		],
 		bbar: Ext.create('Ext.PagingToolbar', {
-			store: store,
+			store: w_store,
 			displayInfo: true,
 			displayMsg: 'Displaying movies {0} - {1} of {2}',
 			emptyMsg: "No movies to display",
@@ -61,5 +46,5 @@ Ext.onReady(function(){
 		renderTo: 'movie-grid'
 	});
 	// trigger the data store load, we must do it or no data is displayed
-	store.loadPage(1);
+	w_store.loadPage(1);
 });
