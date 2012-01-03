ab797b9e1afdd89fe7a2b864446542e7f78d6c4a
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-03 12:28:49 +0200

    better example

--- python/exercises/programming_new/ex08_paragraphs.py
+++ python/exercises/programming_new/ex08_paragraphs.py
@@ -4,14 +4,15 @@ def lines2paragraphs(lines):
     """Group lines into paragraphs - as lists of lines."""
     paragraph = []
     for line in lines:
-        if not line.strip():
+	line=line.strip()
+        if line=='':
             yield paragraph
             paragraph = []
         else:
             paragraph.append(line)
     # The last paragraph remains
     # (if the input didn't end with an empty line)
-    if paragraph:
+    if len(paragraph)>0:
         yield paragraph
 
 # Bonus: streaming lines->lines reformatter
@@ -19,19 +20,14 @@ def lines2paragraphs(lines):
 
 import textwrap
 
-def reformat(lines):
+def reformat(lines,chars):
     """Generate lines of reformatted paragraphs.
 
     Paragraph in input and output are separated by empty lines.
     """
-    for p in lines2paragraphs(lines):
-        for lines in textwrap.wrap(''.join(p),40):
-	    print lines
+    for para in lines2paragraphs(lines):
+        for line in textwrap.wrap(' '.join(para),chars):
+	    print line
         print
 
-# test
-if __name__ == '__main__':
-    #print list(reformat(['foo', 'bar', 'baz', '', 'quux', 'quuux']))
-    #print ''.join(reformat(open('README.Debian')))
-    for p in lines2paragraphs(open('/usr/share/doc/coreutils/README.Debian')):
-	    print 'got paragraph ',p
+reformat(open('/usr/share/doc/coreutils/README.Debian'),72)
