1bb67ecc90e5f98c9bb5a6bc791e227416133b01
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-07 14:30:08 +0200

    comments

--- php/examples/hello_world.php
+++ php/examples/hello_world.php
@@ -1,3 +1,8 @@
+<!--
+This is an example of a a "php only" script.
+
+	Mark Veltzer <mark@veltzer.net>
+-->
 <?php
 echo "Hello, php World!";
 ?>
--- php/examples/hello_world2.php
+++ php/examples/hello_world2.php
@@ -1,8 +1,10 @@
-<?php
-?>
+<!--
+This is an example of php embedded in an HTML document.
 
+	Mark Veltzer <mark@veltzer.net>
+-->
 <html>
-<body>
-<?php echo "Hello, php World!"; ?>
-</body>
+	<body>
+		<?php echo "Hello, php World!"; ?>
+	</body>
 </html>
