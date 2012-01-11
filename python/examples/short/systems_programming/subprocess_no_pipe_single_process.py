e919b71eeb5c41cf7300051f99320f1c9fd9725e
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-11 16:18:04 +0200

    fixed up the examples a little bit

--- python/examples/short/systems_programming/subprocess_no_pipe_single_process.py
+++ python/examples/short/systems_programming/subprocess_no_pipe_single_process.py
@@ -8,6 +8,10 @@ Find the return code of the process.
 """
 
 import subprocess
+try:
+	p=subprocess.Popen(['no such process','--no-such-option'])
+except:
+	print('yes, got error for it')
 p=subprocess.Popen(['sleep','10'])
 print('in here, async, isnt it?');
 ret=p.wait()
--- python/examples/short/systems_programming/subprocess_pipe_single_process.py
+++ python/examples/short/systems_programming/subprocess_pipe_single_process.py
@@ -9,14 +9,19 @@ This example shows how to call popen and get the return text.
 """
 A function that runs a command in a shell,checks that it succeeded and returns the output of that command
 in case of success. In case of error it will throw an exception
+This is similar to python>=2.7 subprocess.check_output
 """
 import subprocess
-def system_check_output(cmd,input=None,cwd=None,env=None): 
-	pipe=subprocess.Popen(cmd,shell=True,cwd=cwd,env=env,stdout=subprocess.PIPE,stderr=subprocess.STDOUT) 
-	(output,errout)=pipe.communicate(input=input) 
-	status=pipe.returncode 
+def system_check_output(arg):
+	pr=subprocess.Popen(arg,stdout=subprocess.PIPE) 
+	(output,errout)=pr.communicate() 
+	status=pr.returncode 
 	if status:
 		raise ValueError('error in executing',cmd)
 	return output
 
-print(system_check_output('ls -l | grep .'))
+try:
+	system_check_output(['nonexistant','--nonexist'])
+except:
+	print('yes, got exception')
+print(system_check_output(['ls','-l']))
