db16169a487c52370dbade0a50788cc8b0025f17
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-16 13:00:33 +0200

    new example

--- javascript/html5/examples/webworkers/background.js
+++ javascript/html5/examples/webworkers/background.js
@@ -4,6 +4,7 @@ function myonmessage(e) {
 	if(first) {
 		first=false;
 		id=e.data;
+		postMessage('worker got id '+e.data);
 	} else {
 		postMessage(id+' got '+e.data);
 	}
@@ -12,6 +13,6 @@ function myonmessage(e) {
 function trigger() {
 }
 
-postMessage('starting...');
+postMessage('anonymous worker starting...');
 onmessage=myonmessage;
 timer=setTimeout(trigger,2000);
