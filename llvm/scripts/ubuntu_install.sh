a27eb8cee227389dc69482c2935c1f0ca1d407f9
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-07 00:34:08 +0200

    llvm examples

---
+++ llvm/scripts/ubuntu_install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# this scrip will install all the required packages that you need on
+# ubuntu to compile and work with this package.
+
+sudo apt-get install llvm-gcc-4.5 llvm-2.8-runtime
