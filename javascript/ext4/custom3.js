c5c5248ccde53e2885d697e0962fe9d0ae4f43ae
Mark Veltzer <mark.veltzer@gmail.com>
2012-03-01 09:20:06 +0200

    added yet another demo for mega control

--- javascript/ext4/custom3.js
+++ javascript/ext4/custom3.js
@@ -1,29 +1,47 @@
 Ext.onReady(function() {
 	Ext.define('MyPanel', {
 		extend: 'Ext.Panel',
-		layout: 'fit',
+		//layout: 'fit',
+		//cls: 'myclass',
+		layout: {
+			type: 'hbox',
+			//align: 'fit'
+		},
+		//width:250,
+		//height:50,
 		initComponent: function() {
 			this.callParent(arguments);
-			var b_next=Ext.create('Ext.Button',{
-				text: 'next',
-				itemId: 'b_next',
+			var w_minus=Ext.create('Ext.Button',{
+				text: 'minus',
+				itemId: 'w_minus',
 				listeners: {
 					click:function() {
-						console.log('next');
+						var current_value=this.ownerCt.getComponent('w_field').getValue();
+						current_value-=0;
+						current_value-=1;
+						this.ownerCt.getComponent('w_field').setValue(current_value);
 					},
 				},
 			});
-			var b_prev=Ext.create('Ext.Button',{
-				text: 'prev',
-				itemId: 'b_prev',
+			this.add(w_minus);
+			var w_field=Ext.create('Ext.form.field.Text',{
+				itemId: 'w_field',
+				value: 0,
+			});
+			this.add(w_field);
+			var w_plus=Ext.create('Ext.Button',{
+				text: 'plus',
+				itemId: 'w_plus',
 				listeners: {
 					click:function() {
-						console.log('prev');
+						var current_value=this.ownerCt.getComponent('w_field').getValue();
+						current_value-=0;
+						current_value+=1;
+						this.ownerCt.getComponent('w_field').setValue(current_value);
 					},
 				},
 			});
-			this.add(b_prev);
-			this.add(b_next);
+			this.add(w_plus);
 		},
 	});
 	Ext.create('MyPanel',{
