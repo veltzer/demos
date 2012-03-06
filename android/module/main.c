fe2089dd5cf69cd1f41ca380c2bc7dc6be82be30
Mark Veltzer <mark.veltzer@gmail.com>
2012-03-06 13:55:18 +0200

    little fixes

--- android/module/main.c
+++ android/module/main.c
@@ -6,19 +6,19 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mark Veltzer");
-MODULE_DESCRIPTION("Hello World module");
+MODULE_DESCRIPTION("Hello Android module");
 
 // our own functions
 static int __init hello_init(void)
 {
-	printk(KERN_ALERT "in init\n");
+	printk(KERN_ALERT "hello android module\n");
 	return (0);
 }
 
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_ALERT "in exit\n");
+	printk(KERN_ALERT "goodbye android module\n");
 }
 
 module_init(hello_init);
