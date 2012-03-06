1f37d90edb5e56a2697388ba6713a2ff091052f5
Mark Veltzer <mark.veltzer@gmail.com>
2012-03-06 16:17:05 +0200

    more examples

--- android/module/copy_ko_to_target.sh
+++ android/module/copy_ko_to_target.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export TOOLS=~/install/android_platform/android-sdk-linux-r16/platform-tools/
 export PATH=$TOOLS:$PATH
-adb push hello.ko /data
+~/install/android_platform/android-sdk-linux-r16/platform-tools/adb push hello.ko /data
