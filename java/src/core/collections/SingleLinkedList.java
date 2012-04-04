c6a1f8b530c75b0e4be83fc8f1a82fe34a2a3dfb
Mark Veltzer <mark.veltzer@gmail.com>
2012-04-04 19:50:32 +0300

    another sample

---
+++ java/src/core/collections/SingleLinkedList.java
@@ -0,0 +1,68 @@
+package core.collections;
+
+/**
+ * This is a simple single linked list implemented in java
+ * without the aid of the java collection package.
+ * 
+ * The drawback of this implementation is that order of
+ * iteration is different than order of insertion.
+ * 
+ * @author mark
+ */
+
+public class SingleLinkedList {
+    private class SingleLinkedListElem {
+    	private Object data;
+    	private SingleLinkedListElem next;
+    	
+    	private SingleLinkedListElem(Object idata,SingleLinkedListElem inext) {
+    		data=idata;
+    		next=inext;
+    	}
+    }
+    public class Iterator {
+    	private SingleLinkedListElem curr;
+    	private Iterator(SingleLinkedListElem icurr) {
+    		curr=icurr;
+    	}
+    	Object getData() {
+    		return curr.data;
+    	}
+    	boolean hasNext() {
+    		return curr!=null;
+    	}
+    	void next() {
+    		curr=curr.next;
+    	}
+    }
+    private int size;
+    private SingleLinkedListElem first;
+    public SingleLinkedList() {
+    	first=null;
+    	size=0;
+    }
+    public void add(Object data) {
+    	first=new SingleLinkedListElem(data,first);
+    	size++;
+    }
+    public int size() {
+    	return size;
+    }
+    public Iterator getIterator() {
+    	return new Iterator(first);
+    }
+    
+    public static void main(String[] args) {
+    	SingleLinkedList li=new SingleLinkedList();
+    	li.add(45);
+    	li.add(23);
+    	li.add(75);
+    	System.out.println("size of the list is "+li.size());
+        SingleLinkedList.Iterator it=li.getIterator();
+        while(it.hasNext()) {
+        	Integer i=(Integer)it.getData();
+        	System.out.println("i is "+i);
+        	it.next();
+        }
+    }
+}
