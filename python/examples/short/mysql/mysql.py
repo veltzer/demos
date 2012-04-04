649a6db4771cfafc97095f17ff3df1a2ed1a9177
Mark Veltzer <mark.veltzer@gmail.com>
2012-04-04 16:15:32 +0300

    mysql example

---
+++ python/examples/short/mysql/mysql.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import MySQLdb # for .connect
+import sys # for .exit
+
+try:
+	conn=MySQLdb.connect(
+		host='localhost',
+		port=3306,
+		user='',
+		passwd='',
+		db='mysql',
+	);
+	with conn:
+		cur=conn.cursor()
+		cur.execute("SELECT VERSION()")
+		data = cur.fetchone()
+		print "Database version : %s " % data
+except MySQLdb.Error, e:
+	print "Error %d: %s" % (e.args[0],e.args[1])
+	sys.exit(1)
