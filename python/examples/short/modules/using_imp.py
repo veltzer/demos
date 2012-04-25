404f156466bcf1c064bd2d496703953ebbc172a0
Mark Veltzer <mark.veltzer@gmail.com>
2012-04-26 02:19:18 +0300

    more python examples

--- python/examples/short/modules/using_imp.py
+++ python/examples/short/modules/using_imp.py
@@ -2,6 +2,8 @@
 
 """
 Demo to show the usage of the imp module in python
+This clearly shows that you can reload any module into any name space using
+the imp module.
 
 	Mark Veltzer <mark@veltzer.net>
 """
@@ -13,3 +15,8 @@ imp.load_source('__main__','using_imp_one.py')
 print('add(2,2) is',add(2,2))
 imp.load_source('__main__','using_imp_two.py')
 print('add(2,2) is',add(2,2))
+
+import math
+print('math.sin(0.5) is',math.sin(0.5))
+imp.load_source('math','using_imp_three.py')
+print('math.sin(0.5) is',math.sin(0.5))
