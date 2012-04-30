5c565a0255a77f113a93d040765d9140c4cf2540
Mark Veltzer <mark.veltzer@gmail.com>
2012-05-01 01:22:31 +0300

    more reflection

--- java/src/core/reflection/SimpleReflection.java
+++ java/src/core/reflection/SimpleReflection.java
@@ -7,7 +7,8 @@ import java.util.List;
 public class SimpleReflection {
     public static void main(String[] args) {
         List<Integer> mylist=new ArrayList<Integer>();
-        Class<? extends List> c=mylist.getClass();
+        @SuppressWarnings("rawtypes")
+		Class<? extends List> c=mylist.getClass();
         TypeVariable<?> types[]=c.getTypeParameters();
         System.out.println(types[0].getName());
     }
