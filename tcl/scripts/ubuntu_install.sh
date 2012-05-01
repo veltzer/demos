67b38e6bbbe04693eee3e393bbb7f5ffcde5db61
Mark Veltzer <mark.veltzer@gmail.com>
2012-05-01 14:09:16 +0300

    better script

--- tcl/scripts/ubuntu_install.sh
+++ tcl/scripts/ubuntu_install.sh
@@ -3,4 +3,5 @@
 # this scrip will install all the required packages that you need on
 # ubuntu to compile and work with this package.
 
-sudo apt-get install tk tcl
+sudo apt-get install tk tcl tcl-doc tcl8.5 tcl8.5-dev tcl8.5-doc
+#sudo apt-get install tcl8.4 tcl8.4-dev tcl8.4-doc
