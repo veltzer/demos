a9e94be1be5aeca42fd14aa7411c09b723d4c119
Mark Veltzer <mark.veltzer@gmail.com>
2012-10-22 12:46:36 +0200

    reorg

--- python/exercises/interbit_basic_new/ex04_min_max_avg.py
+++ python/exercises/interbit_basic_new/ex04_min_max_avg.py
@@ -14,3 +14,15 @@ def max_min_avg_2(lst):
 	return max(lst), min(lst), sum(lst)/len(lst)
 print max_min_avg_2([3,4,5])
 print max_min_avg_2((3,4,5))
+def max_min_avg_3(seq):
+	mysum=0
+	mymin=None
+	mymax=None
+	for v in seq:
+		if mymin is None or v<mymin:
+			mymin=v
+		if mymax is None or v>mymax:
+			mymax=v
+		mysum+=v
+	return mymax, mymin, mysum/len(seq)
+print max_min_avg_3([3,4,5])
