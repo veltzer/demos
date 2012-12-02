8783eb89b49765eee33a7f1b3f912ab4fbf29d29
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-02 10:15:10 +0200

    a few more examples

---
+++ shell/bash/examples/for_break.bash
@@ -0,0 +1,14 @@
+#!/bin/bash -u
+
+# this script shows how to break in the middle of a for loop
+# in case anything goes wrong.
+# give it a list of filenames that exist and some that dont
+
+for x in $@
+do
+	echo "doing $x"
+	stat "$x" 2> /dev/null 1> /dev/null
+	if [[ $? -ne 0 ]]; then
+		exit $?
+	fi
+done
