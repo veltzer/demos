9ad33e222b09915568bc7690f1b9c957554e3377
Mark Veltzer <mark.veltzer@gmail.com>
2012-12-29 00:56:03 +0200

    better mako demos

--- python/examples/long/mako/mako_wrapper.py
+++ python/examples/long/mako/mako_wrapper.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 import sys
-import mako.template
-import mako.lookup
+import mako.template # for mako.template.Template
+import mako.lookup # for mako.lookup.TemplateLookup
 import os # for os.chmod, os.unlink
+import datetime # for now
 
 if len(sys.argv)!=3:
-	raise ValueError('command line issue')
+	print(sys.argv[0]+': usage: '+sys.argv[0]+' [input] [output]',file=sys.stderr)
+	sys.exit(1)
 
 input_encoding='utf-8'
 output_encoding='utf-8'
 p_input=sys.argv[1]
 p_output=sys.argv[2]
-common='src/include/common.makoi'
+
+def years(x):
+	curr_year=datetime.datetime.now().year
+	return ','.join(map(str,range(x,curr_year)))
 
 try:
 	os.unlink(p_output)
@@ -20,19 +26,17 @@ except:
 	# handle the error better, only non existant file should be glossed over...
 	pass
 mylookup=mako.lookup.TemplateLookup(directories=['.'],input_encoding=input_encoding,output_encoding=output_encoding)
-template=mako.template.Template(filename=common,lookup=mylookup,output_encoding=output_encoding,input_encoding=input_encoding)
+template=mako.template.Template(filename=p_input,lookup=mylookup,output_encoding=output_encoding,input_encoding=input_encoding)
 file=open(p_output,'w')
 # python 3
 #file.write((template.render_unicode(attributes={})))
 # python 2
 attr={}
-attr['files']=[ p_input ]
-attr['book']=False
-attr['toc']=False
-attr['inline']=True
-attr['midi']=True
-attr['parts']=False
-file.write(template.render(attributes=attr))
+attr['blocks']=p_input
+attr['list']=['/mako_tests/song1.mk','/mako_tests/song2.mk']
+attr['foo']='bar'
+#file.write(template.render(attributes=attr))
+file.write(template.render(foo='bar',years=years))
 file.close()
 # python 3
 #os.chmod(p_output,0o0444)
