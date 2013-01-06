584fd7aebe2f829c4236c2536fab3414848cad52
Mark Veltzer <mark.veltzer@gmail.com>
2013-01-06 09:51:08 +0200

    minor fixes to the min_max_avg exercises

--- python/exercises/interbit_basic_old/05_functions/min_max_avg_2.py
+++ python/exercises/interbit_basic_old/05_functions/min_max_avg_2.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
+from __future__ import division
+
 def my_min(list):
 	min=list[0]
-	for x in list[1,]:
+	for x in list:
 		if x<min:
 			min=x
 	return min
 
 def my_max(list):
 	max=list[0]
-	for x in list[1,]:
+	for x in list:
 		if x>max:
 			max=x
 	return max
@@ -21,7 +23,6 @@ def my_sum(list):
 	return sum
 
 def min_max_avg(list):
-	return my_min(list),my_max(list),float(my_sum(list))/len(list)
+	return my_min(list),my_max(list),my_sum(list)/len(list)
 
-l=range(0,100000)
-print min_max_avg(l)
+print min_max_avg(xrange(100000))
