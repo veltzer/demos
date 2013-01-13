455e9edc8449624c5c365db464849855abe0acb6
Mark Veltzer <mark.veltzer@gmail.com>
2013-01-13 13:14:13 +0200

    more examples

--- python/exercises/interbit_basic_old/03_basic_statements/digits_2.py
+++ python/exercises/interbit_basic_old/03_basic_statements/digits_2.py
@@ -2,7 +2,13 @@
 
 s=raw_input("Please enter a line of digits: ")
 l=[0]*10
+already_moron=False
 for d in s:
 	if d.isdigit():
 		l[int(d)]+=1
-print l
+	else:
+		if not already_moron:
+			print "you moron"
+			already_moron=True
+if not already_moron:
+	print l
