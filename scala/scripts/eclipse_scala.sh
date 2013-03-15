2fa4931b58640ba10380be2349c5359f881cfdba
Mark Veltzer <mark.veltzer@gmail.com>
2013-03-15 15:19:40 +0200

    first eclipse scala project

--- scala/scripts/eclipse_scala.sh
+++ scala/scripts/eclipse_scala.sh
@@ -2,4 +2,4 @@
 
 # before running this script make sure that you have eclipse for java installed
 # in ~/install/eclipse-java (may be soft link).
-~/install/eclipse-scala/eclipse -data ~/workspace-scala &
+~/install/eclipse-scala/eclipse -data ~/workspace-scala -vmargs -Xmx1524M 1> /dev/null 2> /dev/null &
