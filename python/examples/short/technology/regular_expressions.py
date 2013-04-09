7be46f7653b37585d706ef832d2c998961964237
Mark Veltzer <mark.veltzer@gmail.com>
2013-04-10 00:56:43 +0300

    moved to python3

--- python/examples/short/technology/regular_expressions.py
+++ python/examples/short/technology/regular_expressions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 """
 This is a sample for using regular expressions in python
@@ -7,10 +7,10 @@ This is a sample for using regular expressions in python
 """
 import re # for .compile,.finditer
 
-c=re.compile('^foobar (\d+)$')
+c=re.compile('^\tfoobar (\d+)\n$')
 
 # lets get the match object
-m=c.match('foobar 17')
+m=c.match('\tfoobar 17\n')
 if m:
 	print('m.group() is ',m.group())
 	print('m.group(1) is ',m.group(1))
