d22fbcd42213a2fe4bcf4792183ddbcada086f8b
Mark Veltzer <mark.veltzer@gmail.com>
2013-04-27 18:03:32 +0300

    made this example nicer

--- python/examples/short/technology/regular_expressions.py
+++ python/examples/short/technology/regular_expressions.py
@@ -12,7 +12,7 @@ c=re.compile('^\tfoobar (\d+)\n$')
 # lets get the match object
 m=c.match('\tfoobar 17\n')
 if m:
-	print('m.group() is ',m.group())
-	print('m.group(1) is ',m.group(1))
+	print('m.group() is [{g}]'.format(g=m.group()))
+	print('m.group(1) is [{g}]'.format(g=m.group(1)))
 else:
 	print('no match')
