40b0d7fb8aaf94f8707b142b8bee7f748a79976f
Mark Veltzer <mark.veltzer@gmail.com>
2013-05-23 13:17:17 +0300

    added question

---
+++ java/servlets/Servlets12-filterting/src/exercise/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+
+package exercise;
+
+import javax.servlet.*;
+import java.io.*;
+
+/**
+ *
+ * @author  rank
+ * @version
+ */
+@SuppressWarnings("serial")
+public class HelloWorldServlet extends GenericServlet {
+    
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.getOutputStream().println("Hello World");
+	}
+}
---
+++ java/servlets/Servlets12-filterting/src/exercise/SecurityFilter.java
@@ -0,0 +1,35 @@
+package exercise;
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class SecurityFilter implements Filter {
+
+	private String validUser;
+
+	public void init(FilterConfig cfg) throws ServletException {
+		validUser = cfg.getInitParameter("validUser");
+		if (validUser==null) {
+			throw new ServletException("Must supply a 'validUser' init-param.");
+		}
+	}
+
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+		throws IOException, ServletException {
+		
+		String username = req.getParameter("username");
+		if (username!=null && username.equals(validUser)) {
+			chain.doFilter(req,res);
+		} else {
+			((HttpServletResponse) res).sendError(403,"I don't know you!");
+		}	
+	}
+
+	public void destroy() {
+
+	}
+
+}
