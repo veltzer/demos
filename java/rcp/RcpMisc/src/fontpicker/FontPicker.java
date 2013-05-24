e304ffb8088ace379a00fb9104bb4cf6e91d0af5
Mark Veltzer <mark.veltzer@gmail.com>
2013-05-24 12:24:55 +0300

    fixed up class paths

--- java/rcp/RcpMisc/src/fontpicker/FontPicker.java
+++ java/rcp/RcpMisc/src/fontpicker/FontPicker.java
@@ -4,7 +4,7 @@
  * TODO To change the template for this generated file go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-package com.amdocs.course.swt;
+package fontpicker;
 
 import org.eclipse.swt.*;
 import org.eclipse.swt.events.ModifyEvent;
