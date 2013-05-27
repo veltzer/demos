305eda71a9beb062c705df1c350cbc4c319891eb
Mark Veltzer <mark.veltzer@gmail.com>
2013-05-27 19:12:27 +0300

    check_all passes

--- python/examples/short/iteration/group_by.py
+++ python/examples/short/iteration/group_by.py
@@ -4,9 +4,9 @@ import sys
 import itertools
 
 def indentation(line):
-	return len(line) - len(line.lstrip())
+	return len(line)-len(line.lstrip())
 
 for (indent, paragraph) in itertools.groupby(sys.stdin, key=indentation):
-	print("== %d-spaced paragraph ==" % indent)
+	print("%d-spaced paragraph"%indent)
 	for line in paragraph:
 		print(line.strip())
