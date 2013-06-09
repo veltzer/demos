02d3ebd152f072397dca733cf92aadb07d03593c
Mark Veltzer <mark.veltzer@gmail.com>
2013-06-09 11:14:44 +0300

    reorg of the examples

---
+++ python/examples/short/filesystem/oswalk_simple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This is an example of the 'os.walk' API that allows one to traverse
+a directory of files recursivly.
+This is used to implement find(1)+grep(1) in just a few lines of python.
+
+	Mark Veltzer <mark@veltzer.net>
+"""
+import os # for .path.join, .walk
+import sys # for .argv
+
+if len(sys.argv)<2:
+	raise ValueError("plase pass root_folder")
+root_folder=sys.argv[1]
+
+for root,dirs,files in os.walk(root_folder):
+	for file in files:
+		full=os.path.join(root,file)
+		print(full)
