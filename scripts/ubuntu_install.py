264b6fc6a4980fe9c40107276506fc7d77f9d8df
Mark Veltzer <mark.veltzer@gmail.com>
2014-06-29 15:04:41 +0300

    better install script

---
+++ scripts/ubuntu_install.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# this script will install all the required packages that you need on
+# ubuntu to compile and work with this package.
+
+# TODO:
+
+import subprocess # for check_call()
+
+packs=[
+	'scons',
+	'scons-doc',
+	'pipemeter',
+	'blktool',
+]
+
+args=['sudo','apt-get','install']
+args.extend(packs)
+subprocess.check_call(args)
