fc9f51cf02500f62b651be63a283dfa79e111330
Mark Veltzer <mark.veltzer@gmail.com>
2014-09-18 16:38:06 +0300

    more examples and fixing up some old ones

--- shell/bash/examples/nag.bash
+++ shell/bash/examples/nag.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -u
 
 while [[ true ]]; do
 	echo nag nag...
--- shell/bash/examples/wait.bash
+++ shell/bash/examples/wait.bash
@@ -2,19 +2,21 @@
 
 # An example of using the bash 'wait' function to wait for background processes
 # to terminate...
-# conclusions: wait waits for ALL background processes to end
 #
 #	Mark Veltzer
 
 echo "running sleep 5 in the background..."
 sleep 5 &
 PID5=$!
+echo "the pid of 5 is $PID5..."
 echo "running sleep 10 in the background..."
 sleep 10 &
 PID10=$!
+echo "the pid of 10 is $PID10..."
 echo "going to wait for sleep 5..."
 wait $PID5
-echo "yes, 5 is done"
+echo "yes, 5 is done and status is $?"
 echo "going to wait for sleep 10..."
 wait $PID10
+echo "yes, 10 is done and status is $?"
 echo "done waiting..."
