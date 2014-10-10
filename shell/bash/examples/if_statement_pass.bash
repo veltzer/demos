5ab3baa021db1e5addb1c39b21bc5fa8c85dffb9
Mark Veltzer <mark.veltzer@gmail.com>
2014-10-10 13:38:47 +0300

    another example

--- shell/bash/examples/if_statement_pass.bash
+++ shell/bash/examples/if_statement_pass.bash
@@ -3,6 +3,9 @@
 # this example shows the equivalent of a 'pass' statement
 # in bash. This is the true shell builtin which executes really
 # quickly and succeeds.
+#
+# References:
+# http://stackoverflow.com/questions/2421586/what-is-the-bash-equivalent-of-pythons-pass-statement
 
 if [[ $PATH = "foo" ]]
 then
