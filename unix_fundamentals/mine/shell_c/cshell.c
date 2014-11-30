a0779f64af7626803efd64d5227a0f366330f570
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-30 13:16:45 +0200

    fixed up all permissions (blah!)

--- unix_fundamentals/mine/shell_c/cshell.c
+++ unix_fundamentals/mine/shell_c/cshell.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<stdio.h>	// for fgets(3), stdin, feof(3), printf(3), perror(3)
+#include<stdlib.h>	// for EXIT_FAILURE, EXIT_SUCCESS
 
 const int size=100;
 
@@ -9,12 +9,26 @@ void show_prompt() {
 
 int main(int argc,char** argv,char** envp) {
 	char cmd[size];
+	char* ret;
+	int sys_ret;
 	show_prompt();
-	fgets(cmd,size,stdin);
-	while(!feof(stdin)) {
-		system(cmd);
-		show_prompt();
-		fgets(cmd,size,stdin);
+	ret=fgets(cmd,size,stdin);
+	if(ret==NULL) {
+		perror("problem with fgets");
+		return EXIT_FAILURE;
 	}
-	return(0);
+	while(!feof(stdin)) {
+		sys_ret=system(cmd);
+		if(sys_ret==-1) {
+			perror("problem with system(3)");
+			return EXIT_FAILURE;
+		}
+		show_prompt();
+		ret=fgets(cmd,size,stdin);
+		if(ret==NULL) {
+			perror("problem with fgets");
+			return EXIT_FAILURE;
+		}
+	}
+	return EXIT_SUCCESS;
 }
