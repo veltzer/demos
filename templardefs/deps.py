346c9b1e15f8c7c8d2e6b65d944a1396abca8f0b
Mark Veltzer <mark.veltzer@gmail.com>
2016-11-04 13:22:01 +0200

    apt.yaml -> deps.py

---
+++ templardefs/deps.py
@@ -0,0 +1,54 @@
+'''
+dependencies for this project
+'''
+
+def populate(d):
+    d.packs=[
+        # console fun
+        'cowsay',
+        'figlet',
+        'sl',
+        # xpath
+        'libxml-xpath-perl',
+        # llvm
+        'llvm-3.8',
+        'llvm-3.8-doc',
+        'llvm-runtime',
+        # scala
+        # for scala(1), scalac(1), scalap(1), scaladoc(1) and more...
+        'scala',
+        # csharp
+        'mono-runtime',
+        'monodevelop',
+        # android
+        #'gcc-arm-linux-gnueabi
+        # these are old versions which are no longer supported on ubuntu
+        #'gcc-4.5-arm-linux-gnueabi
+        #'gcc-4.4-arm-linux-gnueabi
+        'gperf',
+        'git-core',
+        'gnupg',
+        'flex',
+        'bison',
+        'gperf',
+        'build-essential',
+        'zip',
+        'curl',
+        'zlib1g-dev',
+        'libc6-dev',
+        'libncurses5-dev',
+        'x11proto-core-dev',
+        'libx11-dev',
+        #'libreadline-gplv2-dev',
+        'zlib1g-dev',
+        'libgl1-mesa-dev',
+        'g++-multilib',
+        'tofrodos',
+        # these I can't find in ubuntu using apt-cache search. Fortunately, the build works:
+        #'ia32-libs',
+]
+
+def getdeps():
+    return [
+        __file__, # myself
+    ]
