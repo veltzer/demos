9d5ec23feaf15c40ff3600686e7544e096422028
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:50:54 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,15 +1,4 @@
-project_github_username = "veltzer"
-project_name="demos"
-project_long_description="Demos for various stuff"
-project_short_description=project_long_description
-project_year_started=2010
-project_description="Various demos"
-project_website="https://veltzer.github.io/demos"
-# keywords to put on html pages or for search, dont put the name of the project or my details
-# as they will be added automatically...
-project_keywords=[
+description_short="Demos for various stuff"
+keywords=[
     "demos",
 ]
-project_paypal_donate_button_id="E3RJEDGT3T32N"
-project_google_analytics_tracking_id="UA-80925041-1"
-codacy_id = None
