5c153644d00fb67d4dec6a821582b78af54f1920
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-03 06:33:39 +0200

    upgrade to 23.10

--- config/deps.py
+++ config/deps.py
@@ -53,6 +53,8 @@ if hasattr(platform, "freedesktop_os_release"):
         llvm_ver = 14
     if VERSION_ID == "23.04":
         llvm_ver = 14
+    if VERSION_ID == "23.10":
+        llvm_ver = 17
     assert llvm_ver is not None
 
 packages.extend([
